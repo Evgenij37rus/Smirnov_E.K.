791b034747c9005ac31420872333c3b273821716
Evgenij37rus <99926974+Evgenij37rus@users.noreply.github.com>
2023-05-23 18:29:53 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Подключение Bootstrap-->
+    <!-- Подключение Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Подключение стилей-->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<div class="header">
+    <h1 style ="text-align:center;">Это главная страница</h1>
+</div>
+<div style= "text-align:center; margin-left:40%;">
+<nav >
+  <ul class="nav">
+        <li class = "nav-item">
+            <a class="nav-link active" href ="http://localhost/SDO_2023/register/register.php">регистрация</a>
+        </li>
+        <li class = "nav-item">
+            <a class="nav-link active" href ="http://localhost/SDO_2023/login/login.php">авторизация</a>
+        </li>
+    </ul>
+</nav>
+</div>
+		
+</body>
+</html>
---
+++ task/task.php
@@ -0,0 +1,35 @@
+<?php 
+  
+  session_start();
+
+    $title = "Скрытая страница";
+    $page_title = "Список задач (скрытая страница)";
+
+     // Имя пользователя root
+     $user = 'root';
+     $password = '';
+     
+    // Имя базы данных 
+     $database = 'SDO_2023';
+     
+     // Сервер является локальным
+     $servername='localhost';
+     $mysqli = new mysqli($servername, $user, $password, $database);
+     
+     // Проверка соединения
+     if ($mysqli->connect_error) {
+         die('Connect Error (' .
+         $mysqli->connect_errno . ') '.
+         $mysqli->connect_error);
+     }
+
+
+   
+    
+    // SQL-запрос для выбора данных из базы данных
+    $sql = " SELECT * FROM tasks";
+    $result = $mysqli->query($sql);
+    $mysqli->close();
+
+  include("../components/layout.php");
+?>
